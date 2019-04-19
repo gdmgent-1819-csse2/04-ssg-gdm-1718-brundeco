eaee182f405b46fe56692e2d67f11cc876148085
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2019-04-19 23:21:33 +0200

    Site publish

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,12 +1,16 @@
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m;
 
 exports.components = {
-  "component---src-templates-blog-post-js": () => import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () =>
+    import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-templates-blog-post-js": () =>
+    import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
+  "component---src-pages-404-js": () =>
+    import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () =>
+    import("/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+};
 
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/.cache/data.json")
-
+exports.data = () =>
+  import(/* webpackChunkName: "pages-manifest" */ "/Users/brunodecoene/Documents/GitHub/04-ssg-gdm-1718-brundeco/.cache/data.json");
